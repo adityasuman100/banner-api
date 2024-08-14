c2865f8ff5a2d16144bd6dcb3c5b9c18afdcc1db
Aditya <aditya.suman@pinelabs.com>
2024-08-15 01:48:33 +0530

    test

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
 import cors from 'cors';
+// import { configDotenv } from "dotenv";
+import dotenv from 'dotenv';
+
+dotenv.config();
+// configDotenv();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'banner_db',
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // Get the latest banner
